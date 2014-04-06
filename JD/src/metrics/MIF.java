8010d5cd9978db0e8cfd68e2227b4bc5488c3dc2
aammar <amir.m.ammar@gmail.com>
2014-04-06 18:03:53 -0400

    modifyoing MIF calcultions to include both MI and MA values

--- JD/src/metrics/MIF.java
+++ JD/src/metrics/MIF.java
@@ -11,19 +11,57 @@ import ast.Access;
 import ast.inheritance.*;
 import ast.ClassObject;
 import ast.FieldObject;
+import ast.MethodObject;
 import ast.SystemObject;
 import ast.TypeObject;
 
 public class MIF {
 	
-	
+	double totalMI = 0 ;
+	double totalMA =0;
 	public  MIF(SystemObject system)
 	{
 		 Set<ClassObject> classes = system.getClassObjects();
 		 InheritanceDetection inheritanceDetection = new InheritanceDetection(system);
 		 for (ClassObject classObject : classes) {
-			 InheritanceTree inheritanceTree = inheritanceDetection.getTree(classObject.getName());
-			 list< 
+			 ClassObject superClass = system.getClassObject(classObject.getSuperclass().getClassType());
+			 if( superClass != null)
+			 {
+				 List<MethodObject> methods = superClass.getMethodList();
+				 for (MethodObject method : methods)
+				 {
+					 if(method.isStatic())
+						 methods.remove(method);
+					 if(method.getSignature().contains("final"))
+						 methods.remove(method);
+				 }
+				 double numberOfOverridableMethods =  methods.size();
+				  double Mi = numberOfOverridableMethods- CheckOverridingMethods(classObject);
+				  totalMI += Mi;
+				  totalMA += classObject.getMethodList().size()- numberOfOverridableMethods + Mi;
+				  
+				 
+				 
+			 }
 		}
+		 
+	}
+	
+	private int CheckOverridingMethods(ClassObject classObject) 
+	{
+		
+		List<MethodObject> methods = classObject.getMethodList();
+        double countingOverridings=0;
+        double countingNewmethods=0;
+
+		for(int i=0; i<methods.size(); i++)
+		{
+			MethodObject mI = methods.get(i);
+			if (mI.overridesMethod())
+			{
+				countingOverridings+=1;
+			}
+		}
+			return countingOverridings;
 	}
 }
