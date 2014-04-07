19ccec68552e33c9da77607b5a5b644f76535410
xemeemax <xemeemax@hotmail.com>
2014-04-07 17:33:30 -0400

    polymorphism update

--- JD/src/metrics/Polymorphism.java
+++ JD/src/metrics/Polymorphism.java
@@ -5,16 +5,20 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import javax.swing.tree.DefaultMutableTreeNode;
+import ast.inheritance.*;
 
 import ast.ClassObject;
 import ast.FieldInstructionObject;
 import ast.MethodObject;
 import ast.SystemObject;
+import ast.inheritance.InheritanceDetection;
+import ast.inheritance.InheritanceTree;
 
 public class Polymorphism {
 	
 	private Map<String, Integer> PolymorphismMap;
-
+	private InheritanceDetection DescendedClasses;
 	public Polymorphism(SystemObject system) 
 	{
 		PolymorphismMap = new HashMap<String, Integer>();
@@ -22,16 +26,35 @@ public class Polymorphism {
 		Set<ClassObject> classes = system.getClassObjects();
 		double NumberOfOverRidingMethods=0;
 		double NumberOfNewMethods=0;
+		DescendedClasses = new InheritanceDetection(system);
+		double DC = 0;
+		Double polymorphism = 0;
+		
+		
 		
 		for(ClassObject classObject : classes) 
 		{
 			NumberOfOverRidingMethods+=CheckOverridingMethods(classObject);
 			NumberOfNewMethods += CheckONewMethods(classObject);
+			NumberOfNewMethods += classObject.getNumberOfMethods() - NumberOfOverRidingMethods;
+			NumberOfDescendedClasses (classObject);
+			DC += CheckDescendedClasses(classObject);
+			
+			
 
 		}
 		// here we should write the formula for Polymorphism Factor.
+		polymorphism = ((NumberOfNewMethods * NumberOfOverRidingMethods) / (NumberOfNewMethods * DC));
+		System.out.println(polymorphism);
 		
 		
+	}
+
+	private void NumberOfDescendedClasses(ClassObject classObject) {
+		
+		InheritanceTree Tree = DescendedClasses.getTree(classObject.getName());
+		
+		// TODO Auto-generated method stub
 		
 	}
 
@@ -75,4 +98,16 @@ public class Polymorphism {
 			return countingNewmethods;
 	}
 	
+	private int CheckDescendedClasses(ClassObject classObject) 
+	{
+		DefaultMutableTreeNode rootclass;
+		int numberOfChildern=0;
+		InheritanceTree tree = DescendedClasses.getTree(classObject.getName());
+		if(tree!=null)
+		{
+			 rootclass = tree.getRootNode();
+			 numberOfChildern=rootclass.getChildCount();
+		}
+		return numberOfChildern;
 	}
+}
