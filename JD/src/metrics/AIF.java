6ff998f3747cc3157058242c626683c8358f5263
FARHAT <agwaeten@live.com>
2014-04-10 01:00:40 -0400

    Adding Attribute Inheritance Factor "AIF" and PrintTo to AIF class

---
+++ JD/src/metrics/AIF.java
@@ -0,0 +1,79 @@
+package metrics;
+
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.List;
+import java.util.Set;
+
+import org.eclipse.core.resources.IFile;
+
+import ast.Access;
+import ast.inheritance.*;
+import ast.ClassObject;
+import ast.FieldObject;
+import ast.MethodObject;
+import ast.SystemObject;
+import ast.TypeObject;
+
+public class AIF {
+	private ToFile output = new ToFile("Attribute Inheritance Factor","AIF","1") ;
+	double totalAI = 0 ;
+	double totalAA =0;
+	Double AIF;
+	public  AIF(SystemObject system)
+	{
+		 Set<ClassObject> classes = system.getClassObjects();
+		 for (ClassObject classObject : classes) 
+		 {
+			 ClassObject superClass = system.getClassObject(classObject.getSuperclass().getClassType());
+			 if( superClass != null)
+			 {
+				 List<FieldObject> fields = superClass.getFieldList();
+				 for (FieldObject field : fields)
+				 {
+					 if(field.isStatic())
+						 fields.remove(field);
+					 if(field.getVariableDeclaration().toString().contains("final"))
+						 fields.remove(field);
+				 }
+				 double numberOfOverridableFields =  fields.size();
+				  double Ai = numberOfOverridableFields- CheckOverridingAttributes(classObject,fields);
+				  totalAI += Ai;
+				  totalAA += classObject.getMethodList().size()- numberOfOverridableFields + Ai;
+			 }
+		}
+		  AIF= totalAI/totalAA;
+		  AIF =Double.parseDouble(new DecimalFormat("##.###").format(AIF));
+		  System.out.println("the value of AIF->>>>>>"+AIF);
+		  String Convertor= AIF.toString();
+		  csv_print(Convertor);
+			try {
+				output.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		 
+	}
+	
+	private void csv_print(String text)
+	{
+		output.CSVFile_print(text);
+	}
+	private double CheckOverridingAttributes(ClassObject classObject,List<FieldObject> fields) 
+	{
+		
+		List<FieldObject> classFields = classObject.getFieldList();
+        double countingOverridings=0;
+
+		for(int i=0; i<classFields.size(); i++)
+		{
+			FieldObject fI = classFields.get(i);
+			if (fields.contains(fI))
+			{
+				countingOverridings+=1;
+			}
+		}
+			return countingOverridings;
+	}
+}
